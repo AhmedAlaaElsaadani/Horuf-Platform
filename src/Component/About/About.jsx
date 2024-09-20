17f896b52b48f6e4daa656494211bddc739839ac
Ahmed Alaa <ahma4352@gmail.com>
2024-09-20 22:35:04 +0300

    +Home <Part 1>

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Heading1 from "../Heading1/Heading1";
 import { useTranslation } from "react-i18next";
+import style from "./About.module.css";
 
 export default function About() {
   const { t } = useTranslation();
   return (
-    <section id="About">
+    <section id="About" className={style.About}>
       <Heading1 headingText={t("About us")} />
     </section>
   );
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,18 +1,26 @@
-import { section } from 'framer-motion/client'
-import React from 'react'
-
+import Lottie from "lottie-react";
+import React from "react";
+import homeAnimation from "../../assets/animations/Home.json";
+import style from "./Home.module.css";
 export default function Home() {
   return (
-    <section id="Home">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                <h1>Home</h1>
-                </div>
-            </div>
-            
+    <section id="Home" className={style.Home}>
+      <div className="container  ">
+        <div className="row bg-danger">
+          <div className="col-6">
+            <h1>Home</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+              iusto accusamus consectetur iste sapiente eligendi dicta, est hic
+              odit dolorem facere earum libero ea veritatis quibusdam laboriosam
+              asperiores cumque rem.
+            </p>
+          </div>
+          <div className="col-5 offset-1">
+            <Lottie animationData={homeAnimation} />
+          </div>
         </div>
-
+      </div>
     </section>
-  )
+  );
 }
